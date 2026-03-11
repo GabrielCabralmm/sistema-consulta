d2dac4e28b77ce813753a0144f8dcc3470063d37
GabrielCabralmm <gabriel.cmmariano@gmail.com>
2026-03-11 19:57:43 -0300

    Adicionando as funções adicionais dentro do componente ConsultaCard.

--- src/components/ConsultaCard.tsx
+++ src/components/ConsultaCard.tsx
@@ -13,7 +13,27 @@ export default function ConsultaCard({
   onConfirmar,
   onCancelar,
 }: ConsultaCardProps) {
+
+    function formatarValor(valor: number): string {
+    return valor.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    });
+  }
   
+  // Auxiliar LOCAL - formata data para 25/03/2026
+  function formatarData(data: Date): string {
+    return data.toLocaleDateString("pt-BR");
+  }
+  
+  return (
+    <View style={styles.card}>
+      <Text>{consulta.paciente.nome}</Text>
+      <Text>{formatarData(consulta.data)}</Text>
+      <Text>{formatarValor(consulta.valor)}</Text>
+    </View>
+  );
+   
   return (
     <View style={styles.card}>
       <Text>{consulta.paciente.nome}</Text>
